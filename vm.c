78cad52381f2e8fa1a2501a4820ba24bc319a538
Carlos Manrique Ortega <cjmortega@gmail.com>
2024-07-31 10:37:39 +0200

    chapter 19 complete, fix addition bug

--- vm.c
+++ vm.c
@@ -129,7 +129,7 @@ static InterpretResult run() {
                 } else if (IS_NUMBER(peek(0)) && IS_NUMBER(peek(1))) {
                     double b = AS_NUMBER(pop());
                     double a = AS_NUMBER(pop());
-                    push(NUMBER_VAL(a = b));
+                    push(NUMBER_VAL(a + b));
                 } else {
                     runtimeError("Operands must be two numbers or two strings.");
                     return INTERPRET_RUNTIME_ERROR;
